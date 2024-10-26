4304f3c97adb7d601ecbba25f7ca8951127e6b3e
Color-Kat <katik7979@gmail.com>
2024-10-26 16:29:18 +0300

    init new NextJS 15 project

---
+++ app/some/page.tsx
@@ -0,0 +1,12 @@
+import {NextPage} from 'next';
+
+const SomePage: NextPage = ({}) => {
+    
+    return (
+        <div className="">
+            SomePage
+        </div>
+    );
+};
+
+export default SomePage
